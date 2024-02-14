594cdaf977fb20f9d88115537088eac2bfc782d1
Jamie Perkins <jamie.perkins@gmail.com>
2024-02-13 17:06:14 -0700

    performance

--- ios/Plugin/Performance.swift
+++ ios/Plugin/Performance.swift
@@ -1,8 +1,19 @@
 import Foundation
 
 @objc public class Performance: NSObject {
-    @objc public func echo(_ value: String) -> String {
-        print(value)
-        return value
+
+    var launchStart: Date
+
+    @objc public func launchComplete() -> String {
+        let endDate = Date()
+        let timeInterval = endDate.timeIntervalSince(launchStart)
+        let result = String(format: "🏎️ Launch time: %.2f seconds", timeInterval)
+        print(result)
+        return result
+    }
+
+    override public func load() {
+        print("🏎️ Performance plugin loaded")
+        launchStart = Date()
     }
 }
